3f4b74a0a9b74671dce1ac257f6a96b8b8493dba
malavikaac <acmalavika@gmail.com>
2023-03-29 15:24:10 +0530

    completed portfolie

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -3,13 +3,14 @@ import 'package:url_launcher/url_launcher.dart';
 
 launchMailto() async {
   final mailtoLink = Mailto(
-    to: ['acmalavika@gmail.com'],
-    cc: ['acmalavika19ec@srishakthi.com', 'cc2@example.com'],
+    to: ['to@example.com'],
+    cc: ['cc1@example.com', 'cc2@example.com'],
     subject: 'mailto example subject',
     body: 'mailto example body',
   );
   // Convert the Mailto instance into a string.
   // Use either Dart's string interpolation
   // or the toString() method.
+  // ignore: deprecated_member_use
   await launch('$mailtoLink');
 }
